154076baf7e6e653652de1db26529ccd87c96859
anushka-2626 <anushka1784.be23@chitkara.edu.in>
2025-07-09 11:23:18 +0530

    Loops lab uploaded 9th july

--- student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
+++ student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
@@ -11,7 +11,13 @@
 let lowHR = 110
 let highHR = 125
 var movementHeartRates: [String: Int] = ["Walking": 85, "Running": 120, "Swimming": 130, "Cycling": 128, "Skiing": 114, "Climbing": 129]
-
+for (exercise,heartrate) in movementHeartRates {
+    if heartrate < lowHR || heartrate > highHR {
+        continue
+    }else{
+        print("You could go \(exercise)")
+    }
+}
 
 /*:
  _Copyright © 2023 Apple Inc._
