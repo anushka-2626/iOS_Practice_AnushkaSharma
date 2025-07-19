55d273755d6009b3b21a3d527bec03fce3692add
anushka-2626 <anushka1784.be23@chitkara.edu.in>
2025-07-19 16:23:37 +0530

    Second AutoLayout App

---
+++ Apps/Music App/Music AppTests/Music_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppTests.swift
+//  Music AppTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import Music_App
+
+struct Music_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
