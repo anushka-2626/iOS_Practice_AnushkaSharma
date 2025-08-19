a9ba3e111803c5f973c0678b2613884ef4d286d6
anushka-2626 <156744598+anushka-2626@users.noreply.github.com>
2025-08-19 09:08:38 +0530

    Add files via upload

---
+++ Xcode Labs:Apps/Table View/Table ViewTests/Table_ViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  Table_ViewTests.swift
+//  Table ViewTests
+//
+//  Created by Student on 18/08/25.
+//
+
+import Testing
+@testable import Table_View
+
+struct Table_ViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
